2ef32bd94ea0a9a1d071b76efacf4b8dc5df5f38
Kerry Buckley <kerryjbuckley@gmail.com>
2020-04-21 20:36:34 +0100

    And … er … done! 😊

--- lib/palindrome_descendant.ex
+++ lib/palindrome_descendant.ex
@@ -1,12 +1,31 @@
 defmodule PalindromeDescendant do
+  require Integer
+
   def palindrome_descendant?(number) do
     number
     |> get_digits()
-    |> palindrome?()
+    |> descendants()
+    |> any_palindromes?()
   end
 
-  defp get_digits(n) when n < 10, do: [n]
-  defp get_digits(n), do: [Integer.mod(n, 10) | get_digits(div(n, 10))]
+  def get_digits(n), do: get_digits([], n)
+  defp get_digits(digits, n) when n < 10, do: [n | digits]
+  defp get_digits(digits, n), do: get_digits([Integer.mod(n, 10) | digits], div(n, 10))
 
-  defp palindrome?(digits), do: digits == Enum.reverse(digits)
+  def descendants(digits), do: Stream.unfold(digits, &descendant/1)
+
+  defp descendant(digits) do
+    cond do
+      Integer.is_odd(length(digits)) -> nil
+      Enum.any?(digits, &(&1 > 9)) -> nil
+      true -> {digits, add_pairs(digits)}
+    end
+  end
+
+  defp add_pairs(digits), do: digits |> Enum.chunk_every(2) |> Enum.map(&Enum.sum/1)
+
+  defp any_palindromes?(stream), do: Enum.any?(stream, &palindrome?/1)
+
+  def palindrome?([_]), do: false
+  def palindrome?(digits), do: digits == Enum.reverse(digits)
 end
--- test/palindrome_descendant_test.exs
+++ test/palindrome_descendant_test.exs
@@ -4,11 +4,47 @@ defmodule PalindromeDescendantTest do
 
   import PalindromeDescendant
 
-  test "reports palindromic numbers as true" do
-    assert palindrome_descendant?(11)
+  describe "PalindromeDescendant.palindrome_descendant?/1" do
+    test "reports palindromic numbers as true" do
+      assert palindrome_descendant?(11)
+    end
+
+    test "reports numbers with palindromic descendants as true" do
+      assert palindrome_descendant?(13_001_120)
+    end
+
+    test "reports numbers that don't reduce to a palindrome as false" do
+      refute palindrome_descendant?(1122)
+    end
   end
 
-  test "reports numbers that don't reduce to a palindrome as false" do
-    refute palindrome_descendant?(1122)
+  describe "PalindromeDescendant.get_digits/1" do
+    test "returns a list of a number's digits" do
+      assert get_digits(1234) == [1, 2, 3, 4]
+    end
+  end
+
+  describe "PalindromeDescendant.descendants/1" do
+    test "returns a stream of descendants (including the original) until it reaches two digits" do
+      assert [1, 1, 2, 1, 1, 2, 3, 0] |> descendants |> Enum.to_list() == [
+               [1, 1, 2, 1, 1, 2, 3, 0],
+               [2, 3, 3, 3],
+               [5, 6]
+             ]
+    end
+  end
+
+  describe "PalindromeDescendant.palindrome?/1" do
+    test "reports palindromic lists of digits as true" do
+      assert palindrome?([1, 2, 3, 2, 1])
+    end
+
+    test "reports non-palindromic lists of digits as false" do
+      refute palindrome?([1, 2, 3, 1])
+    end
+
+    test "reports single-digit lists as false" do
+      refute palindrome?([1])
+    end
   end
 end
