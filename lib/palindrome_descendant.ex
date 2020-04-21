d69a9240372347aa8a4114a06f584ee65052f17b
Kerry Buckley <kerryjbuckley@gmail.com>
2020-04-21 17:50:11 +0100

    Check for simple palindromes

--- lib/palindrome_descendant.ex
+++ lib/palindrome_descendant.ex
@@ -1,5 +1,12 @@
 defmodule PalindromeDescendant do
-  def palindrome_descendant?(_number) do
-    true
+  def palindrome_descendant?(number) do
+    number
+    |> get_digits()
+    |> palindrome?()
   end
+
+  defp get_digits(n) when n < 10, do: [n]
+  defp get_digits(n), do: [Integer.mod(n, 10) | get_digits(div(n, 10))]
+
+  defp palindrome?(digits), do: digits == Enum.reverse(digits)
 end
--- test/palindrome_descendant_test.exs
+++ test/palindrome_descendant_test.exs
@@ -7,4 +7,8 @@ defmodule PalindromeDescendantTest do
   test "reports palindromic numbers as true" do
     assert palindrome_descendant?(11)
   end
+
+  test "reports numbers that don't reduce to a palindrome as false" do
+    refute palindrome_descendant?(1122)
+  end
 end
